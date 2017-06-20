56dcf69217fe8b04dbe02adadab761fff185bc0b
adunionx <natanray321@gmail.com>
2017-06-20 12:44:38 +0530

    Update key.php

--- key.php
+++ key.php
@@ -1,5 +1,5 @@
 <?php
-$a = array('AIzaSyCqnzxuB5MIFSIwaHq27dQvAE9ly4eghCA','AIzaSyD_FPyxPNgk5hnsuc843625pVm9_qMLIQM','AIzaSyDJLjgoEgv5rf4_Co-mnUr6xy9PJkVS3K0','AIzaSyD86jS3gQYzNbxNfsofrtoys0WQUd0PnVw','AIzaSyDM8jUSQioAgNxdplAYO4WQwf-eD86PBtU','AIzaSyC14uuCh8UbFIZ-FKIHYhBlrFrkoN1xfJA','AIzaSyBiKs6E7tKnId7q27fQ5PoBx7Uzqia_1DE','AIzaSyCrs-hIH5jZiad6EIdJdhKTUxu6bTt4KxU','AIzaSyDBkt_CfgJXOcUQuJTF-oZagYtYAoNEMnc','AIzaSyDLSA_3ipXxw11pHNOVW6sR_e1mLYEjOB0');
+$a = array('AIzaSyCsFyLiUQHr9uq5H60IcV_41I-zgDtSX94','AIzaSyCsFyLiUQHr9uq5H60IcV_41I-zgDtSX94','AIzaSyCsFyLiUQHr9uq5H60IcV_41I-zgDtSX94','AIzaSyCsFyLiUQHr9uq5H60IcV_41I-zgDtSX94','AIzaSyCsFyLiUQHr9uq5H60IcV_41I-zgDtSX94','AIzaSyCsFyLiUQHr9uq5H60IcV_41I-zgDtSX94','AIzaSyCsFyLiUQHr9uq5H60IcV_41I-zgDtSX94','AIzaSyCsFyLiUQHr9uq5H60IcV_41I-zgDtSX94','AIzaSyCsFyLiUQHr9uq5H60IcV_41I-zgDtSX94','AIzaSyCsFyLiUQHr9uq5H60IcV_41I-zgDtSX94');
 $b = count($a)-1;
 $key = $a[rand(0,$b)];
 ?>
