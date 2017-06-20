0ebdf95748ffb0512d22b23447357ec7b28cd0ad
adunionx <natanray321@gmail.com>
2017-06-21 03:01:10 +0530

    Update key.php

--- key.php
+++ key.php
@@ -1,5 +1,6 @@
 <?php
-$a = array('AIzaSyCK-lPP3Lf6MlTYin7UEOFIkhOwmyio-fA');
+$a = array('AIzaSyCK-lPP3Lf6MlTYin7UEOFIkhOwmyio-fA','AIzaSyCK-lPP3Lf6MlTYin7UEOFIkhOwmyio-fA','AIzaSyCK-lPP3Lf6MlTYin7UEOFIkhOwmyio-fA','AIzaSyCK-lPP3Lf6MlTYin7UEOFIkhOwmyio-fA','AIzaSyCK-lPP3Lf6MlTYin7UEOFIkhOwmyio-fA','AIzaSyCK-lPP3Lf6MlTYin7UEOFIkhOwmyio-fA','AIzaSyCK-lPP3Lf6MlTYin7UEOFIkhOwmyio-fA','AIzaSyCK-lPP3Lf6MlTYin7UEOFIkhOwmyio-fA','AIzaSyCK-lPP3Lf6MlTYin7UEOFIkhOwmyio-fA','AIzaSyCK-lPP3Lf6MlTYin7UEOFIkhOwmyio-fA');
 $b = count($a)-1;
 $key = $a[rand(0,$b)];
 ?>
+
